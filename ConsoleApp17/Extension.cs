8d64638bbfb35d6ea0b3a06dc42f0e5b5de3246b
unknown <perviz.elizade.2002@mail.ru>
2022-11-27 23:48:48 +0400

    first commit

---
+++ ConsoleApp17/Extension.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp17
+{
+   static class Extension
+    {
+
+        public static string[] CustomSplit(this string sentence, char symbol = ' ')
+        {
+            string str = "";
+            string[] arr = new string[0];
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == symbol)
+                {
+                    Array.Resize(ref arr, arr.Length + 1);
+                    arr[arr.Length - 1] = str;
+                    str = string.Empty;
+                }
+                else
+                {
+                    str += sentence[i];
+                }
+            }
+
+            Array.Resize(ref arr, arr.Length + 1);
+            arr[arr.Length - 1] = str;
+
+            return arr;
+        }
+    }
+}
---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            
+            Console.WriteLine(str.CustomSplit());
+
+        }
+    }
+}
